6ab27d3e79d30933dc354437aa2f70905ebaca96
mihovilrak <mihovil.1978@hotmail.com>
2025-02-03 02:29:39 +0100

    layout type fix 2

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,14 +3,14 @@ import { ThemeProvider } from "next-themes";
 import Navigation from "@/components/Navigation";
 import ScrollToTop from "@/components/ScrollToTop";
 
-type LayoutProps = {
+interface Props {
   children: React.ReactNode;
   params: {
     locale: string;
   };
-};
+}
 
-const Layout = async ({ children, params }: LayoutProps) => {
+export default async function Layout({ children, params }: Props) {
   const locale = params.locale;
   const messages = await import(`../../messages/${locale}.json`)
     .then(module => module.default)
@@ -20,24 +20,22 @@ const Layout = async ({ children, params }: LayoutProps) => {
 
   return (
     <div>
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <div className="min-h-screen flex flex-col">
-              <Navigation />
-              <main className="flex-grow">
-                {children}
-              </main>
-            </div>
-            <ScrollToTop />
-          </ThemeProvider>
-        </NextIntlClientProvider>
-      </div>
+      <NextIntlClientProvider locale={locale} messages={messages}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="min-h-screen flex flex-col">
+            <Navigation />
+            <main className="flex-grow">
+              {children}
+            </main>
+          </div>
+          <ScrollToTop />
+        </ThemeProvider>
+      </NextIntlClientProvider>
+    </div>
   );
 }
-
-export default Layout;
